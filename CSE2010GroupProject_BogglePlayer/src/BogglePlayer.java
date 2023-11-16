4ab0bc2de89e64dc4fa2d5bc7c26b4ef7e8532b9
Anthony Clayton <114684904+AnthonyClayton12@users.noreply.github.com>
2023-11-16 11:26:31 -0500

    Initial commit -
    Adds starting classes needed for project. **Needs email addresses added.

---
+++ CSE2010GroupProject_BogglePlayer/src/BogglePlayer.java
@@ -0,0 +1,57 @@
+/*
+  Authors: 
+  	Clayton, Anthony
+  	Dean, Darian
+	Goembel, Matthew
+  	
+  Email addresses of group members:
+  	aclayton2023@my.fit.edu
+  	
+  Group name: 34b
+
+  Course: CSE 2010
+  Section: 34
+
+  Description of the overall algorithm and key data structures:
+
+
+*/
+
+import java.util.*;
+
+
+public class BogglePlayer
+{
+
+    // initialize BogglePlayer with a file of English words
+    public BogglePlayer(String wordFile)
+    {
+
+    }
+
+    // based on the board, find valid words
+    //
+    // board: 4x4 board, each element is a letter, 'Q' represents "QU", 
+    //    first dimension is row, second dimension is column
+    //    ie, board[row][col]     
+    //
+    // Return at most 20 valid words in UPPERCASE and 
+    //    their paths of locations on the board in myWords;
+    //    Use null if fewer than 20 words.
+    //
+    // See Word.java for details of the Word class and
+    //     Location.java for details of the Location class
+
+    public Word[] getWords(char[][] board)
+    {
+	Word[] myWords = new Word[20];
+
+
+
+
+
+
+        return myWords;
+    }
+
+}
