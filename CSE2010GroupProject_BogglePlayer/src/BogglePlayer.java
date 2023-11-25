c2d89954d4e0eb10a9954faa2df6ee06754d6269
Matthew Goembel <mgoembel1@gmail.com>
2023-11-25 01:51:12 -0500

    Trie creation and search ideas

--- CSE2010GroupProject_BogglePlayer/src/BogglePlayer.java
+++ CSE2010GroupProject_BogglePlayer/src/BogglePlayer.java
@@ -22,15 +22,33 @@
 import java.util.*;
 
 
-public class BogglePlayer
-{
+public class BogglePlayer {
+    // Var alphabet size
+    // Var TrieNode array of children
+    // Var endOfAWord
+    // Var TrieNode rootNode
 
     // initialize BogglePlayer with a file of English words
-    public BogglePlayer(String wordFile)
-    {
-
+    public BogglePlayer(String wordFile) {
+	// Initiate children of root (A-Z)
+	// Set endOfAWord for each node
     }
 
+    // Insert(word) method O(word_length):
+    // 	  Each charchter of a word is an individual Trie node
+    //    For each charcter of the word:
+    // 	      Construct new nodes of the word. Trie depth = word_length.
+    //        If word is a prefix of an existing word/Node already exists, 
+    //        combind non-new nodes as a prefix, mark remainder of word as endOfAWord
+    //        Mark final nodes at endOfAWord
+    // 
+    // Seach(word) method O(word_length):
+    //     Compares current charchter and moves down the trie
+    //     if we reach endOfAWord the word exists --> add points, compare highest 20 words
+    //     if current char DNE, stop searching, word cannot exist
+    // 
+    // 
+    // 
     // based on the board, find valid words
     //
     // board: 4x4 board, each element is a letter, 'Q' represents "QU", 
